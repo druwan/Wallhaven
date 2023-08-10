9077719bad0ce6746fa62a439b5506a139606849
Christopher Vestman <christophervestman@gmail.com>
2023-08-10 09:24:44 +0200

    feat: added node implementation

---
+++ node/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const fs = require('fs');
+
+const fetchWallpapers = async () => {
+  const wallpapersURL = `${process.env.BASE_URL}${process.env.USERNAME}/${process.env.COLLECTION_ID}?apikey=${process.env.API_KEY}`;
+  const res = await fetch(wallpapersURL);
+  const { data } = await res.json();
+
+  let wallpapers = [];
+
+  data.map((wallpaper) => {
+    wallpapers.push(wallpaper);
+  });
+
+  fs.writeFile(
+    'wallpapers.json',
+    JSON.stringify(wallpapers, null, 2),
+    'utf-8',
+    (err) => {
+      if (err) throw err;
+    }
+  );
+};
+
+const wallpapers = fetchWallpapers();
